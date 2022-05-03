4b2d0f7a7d16cebc40c32dcc903ba8e95dda8c90
nilberm <nilber16@outlook.com>
2022-05-03 10:17:59 -0300

    corretions

--- Week-3/calculator.py
+++ Week-3/calculator.py
@@ -21,25 +21,20 @@ import math
 # print(f"The volume of the sphere is: {volumeSphere}")
 
 
-sideSquare = float(
-    input("What is the length of a side of the square in centimeters? "))
+sideSquare = float(input("What is the length of a side of the square in centimeters? "))
 areaSquare = sideSquare ** 2
 volumeSquare = sideSquare ** 3
 
 print(f"The area of the square is: {areaSquare}cm / {areaSquare / 10000}m")
 print(f"The volume of the square is: {volumeSquare}cm")
 
-lengthRectangle = float(
-    input("What is the length of the rectangle in centimeters? "))
-widthRectangle = float(
-    input("What is the width of the rectangle in centimeters? "))
+lengthRectangle = float(input("What is the length of the rectangle in centimeters? "))
+widthRectangle = float(input("What is the width of the rectangle in centimeters? "))
 areaRectangle = lengthRectangle * widthRectangle
 
-print(
-    f"The area of the rectangle is: {areaRectangle}cm / {areaRectangle / 10000}m")
+print(f"The area of the rectangle is: {areaRectangle}cm / {areaRectangle / 10000}m")
 
-radiusCircle = float(
-    input("What is the radius of the circle in centimeters? "))
+radiusCircle = float(input("What is the radius of the circle in centimeters? "))
 areaCircle = math.pi * (radiusCircle**2)
 volumeSphere = (4 * math.pi * (radiusCircle**3)) / 3
 
